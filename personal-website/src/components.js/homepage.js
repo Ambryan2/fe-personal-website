de4b9fc7abb4dc8de81f124350f07f1891e1cd15
Your Name <alobryan2@gmail.com>
2023-01-31 10:31:40 -0500

    layout work

--- personal-website/src/components.js/homepage.js
+++ personal-website/src/components.js/homepage.js
@@ -8,6 +8,25 @@ export const HomePage = () => {
                 <Header />
                 <div className="name">
                     <h1>Angelo Bryan</h1>
+                    <h3>Web Developer</h3>
+                </div>
+            </div>
+            <div className="home-info">
+                <div className="info-text">
+                    <h3>Intro</h3>
+                    <p>Hello my name is angelo </p>
+                </div>
+                <div>
+                    <p>img</p>
+                </div>
+            </div>
+            <div className="home-info">
+                <div>
+                    <p>img</p>
+                </div>
+                <div className="info-text">
+                    <h3>Intro</h3>
+                    <p>Hello my name is angelo </p>
                 </div>
             </div>
         </div>
